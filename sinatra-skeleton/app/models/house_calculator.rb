91e61b41a3b635fbab7086d6e3354d50ab39a2bd
Anne Willborn & C.J. Jameson <pair+awillborn.cjcjameson@devbootcamp.com>
2014-06-07 16:38:37 -0700

    mega hash in, points hash out

--- sinatra-skeleton/app/models/house_calculator.rb
+++ sinatra-skeleton/app/models/house_calculator.rb
@@ -3,31 +3,39 @@ require_relative "house.rb"
 
 class HouseCalculator
 
-  def self.select_house_from_genres(genre_array)
-    initialize_frequency_collector
-    count_house_frequency(genre_array)
-    top_score = @frequency_collector.values.max
-    @frequency_collector.select{|k,v| v==top_score}.keys.sample
+  def self.movie_genre_receiver(movie_hash)
+    movie_house_frequency(movie_hash.values.flatten) # => {H: 3, R: 2, G: 5, S: 4} (accumulated total)
   end
 
-  def self.initialize_frequency_collector
-    @frequency_collector = {}
-    House.house_names.each do |house_name|
-      @frequency_collector[house_name] = 0
-    end
-  end
 
-  def self.count_house_frequency(genre_array)
+  def self.movie_house_frequency(genre_array)
+    house_points = initialize_empty_house_points
     genre_array.each do |genre|
-      @frequency_collector[fetch_house(genre)] += 1
+      house_points[fetch_house(genre)] += 1
     end
-    @frequency_collector
+    return house_points # => {H: 0, R: 1, G: 1, S: 0} or more
   end
 
-  def self.fetch_house(genre)
+  private
+
+  def self.initialize_empty_house_points #HELPER METHOD to start hashes
+    house_points = {}
+    House.house_names.each do |house_name|
+      house_points[house_name] = 0
+    end
+    return house_points # => {H: 0, R: 0, G: 0, S: 0} (zeroed out)
+  end
+
+  def self.fetch_house(genre) #HELPER METHOD to connect to CSV
     CSV.foreach('genres.csv', {headers: true}) do |row|
       return row["house"] if row["genre"] == genre
     end
+  end
+
+  def self.select_house_from_genres(genre_array) #NOT TIED INTO CURRENT LOGIC. Legacy.
+    single_house_points = movie_house_frequency(genre_array)
+    top_score = single_house_points.values.max
+    single_house_points.select{|k,v| v==top_score}.keys.sample # => Returns the top-most house for a specific movie's genre-array. Ties are decided randomly.
   end
 end
 
@@ -41,9 +49,9 @@ assert{HouseCalculator.fetch_house("Film-noir") == "Ravenclaw"}
 assert{HouseCalculator.fetch_house("Crime") == "Slytherin"}
 
 
-test_genres1 = ["Action", "Comedy", "Sci-fi"]
-assert{HouseCalculator.select_house_from_genres(test_genres1) == "Gryffindor"}
+test_hash = {"movie1" => ["Action", "Comedy", "Sci-fi"]}
+assert {HouseCalculator.movie_genre_receiver(test_hash) == {"Gryffindor"=>2, "Slytherin"=>0, "Hufflepuff"=>1, "Ravenclaw"=>0}}
 
-test_genres2 = ["Western", "Comedy"]
-test_houses2 = ["Hufflepuff", "Gryffindor"]
-assert{test_houses2.include?(HouseCalculator.select_house_from_genres(test_genres2))}
+test_hash2 = {"movie2" => ["Western", "Comedy"],
+              "movie3" => ["Western", "Film-noir", "Musical"]}
+assert{HouseCalculator.movie_genre_receiver(test_hash2) == {"Gryffindor"=>2, "Slytherin"=>0, "Hufflepuff"=>2, "Ravenclaw"=>1}}
