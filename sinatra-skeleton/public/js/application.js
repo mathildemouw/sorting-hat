87d86ed6bfe0a3671ec4c57648d48b055208bb35
Apprentice <apprentice@dbc23.local>
2014-06-07 16:39:08 -0700

    Add ajax request to OMDB movie API
      *Collects titles and genres for each movie
      returned in movieList object.
      *And we danced.

--- sinatra-skeleton/public/js/application.js
+++ sinatra-skeleton/public/js/application.js
@@ -2,6 +2,45 @@ $(document).ready(function() {
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
+  // IMDb ID to Search
+  var movieList = {};
+  $('#userMovieInput').submit(function() {
+    event.preventDefault();
+    var imdbTitle =  $('#movieTitle').val();
+    $.ajax({
+      type: 'GET',
+      url: "http://www.omdbapi.com/?i=&t=" + imdbTitle,
+      }).done(function(data) {
+        console.log(data)
+        var genres = data.match(/\"Genre\":\"(.*)\",\"Director\"/)[1]
+        // debugger
+        genres = genres.split(", ");
+        movieList[imdbTitle] = genres;
+        console.log(movieList)
+      }).fail(function(){console.log("you failed")})
+    })
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+    //OMDB request
+    // var imdbTitle = event.movies;
+
+
+
+  $('#sortByMovies').submit(function(event) {
+    movieList
+  })
+
+  // Send Request
+  // var http = new ActiveXObject("Microsoft.XMLHTTP");
+
+  // http.open("GET", "http://www.omdbapi.com/?i=&t=" + imdbTitle, false);
+  // http.send(null);
+
+  // // Response to JSON
+  // var omdbData = http.responseText;
+  // var omdbJSON = eval("(" + omdbData + ")");
+
+  // // Returns Movie Title
+  // alert(omdbJSON.Title);
+  // console.log(omdbJSON.Genre)
+
 });
