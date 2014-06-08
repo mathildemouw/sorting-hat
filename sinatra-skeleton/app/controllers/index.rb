789c5984bc84ade8a23bd87069fef6fd248bb552
Anne Willborn <awillborn@gmail.com>
2014-06-07 19:10:27 -0700

    - Refactor HouseCalculator model - add javascript for Sort Me! - Fix CSV bugs - Add movie titles to DOM

--- sinatra-skeleton/app/controllers/index.rb
+++ sinatra-skeleton/app/controllers/index.rb
@@ -1,7 +1,10 @@
+require 'json'
+
 get '/' do
   erb :index
 end
 
 post '/house' do
-
+  results = HouseCalculator.calculate_total_house_points(params)
+  results.to_json
 end
--- sinatra-skeleton/public/js/application.js
+++ sinatra-skeleton/public/js/application.js
@@ -1,9 +1,18 @@
+var movieList = {}; // {"Fargo": ["Crime", "Comedy"], "The Lion King": ["Drama", "Comedy"] }
 $(document).ready(function() {
+  setupAddMovie();
+  setupGetSorted();
+});
 
-  var movieList = {};
-  $('#userMovieInput').submit(function() {
-    event.preventDefault();
+function appendTitle(title) {
+  $('.movie-list').append("<li>" + title + "</li>")
+}
+
+function setupAddMovie() {
+  $('#userMovieInput').submit(function(e) {
+    e.preventDefault();
     var imdbTitle =  $('#movieTitle').val();
+    appendTitle(imdbTitle);
     $.ajax({
       type: 'GET',
       url: "http://www.omdbapi.com/?i=&t=" + imdbTitle,
@@ -13,8 +22,18 @@ $(document).ready(function() {
         movieList[imdbTitle] = genres;
       }).fail(function(){console.log("Your request was bad and you should feel bad!")})
     })
+  }
 
-  $('#sortByMovies').submit(function(event) {
-    movieList
+function setupGetSorted(){
+  $('.sort').on("click", (function(e) {
+    e.preventDefault()
+    $.ajax({
+      type: "POST",
+      url: "/house",
+      data: movieList,
+    }).done(function(data){
+      console.log(data)
+    })
   })
-});
+  )
+}
